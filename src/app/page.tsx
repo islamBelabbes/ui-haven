d525ee64f54da055e2099a263731adba75503a09
islamBelabbes <belabbesislam2@gmail.com>
2024-07-28 12:26:03 +0100

    add course-card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
+"use client";
+import CourseCard from "@/components/elements/courseCard/course-card";
 import InfoCard from "@/components/elements/infoCard/info-card";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-neutral-100 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-3 bg-neutral-100 text-white">
       <InfoCard />
+      <CourseCard />
     </main>
   );
 }
