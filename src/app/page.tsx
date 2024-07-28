69ca2fe558a851c86f0121ee3b16a5b264515d2b
islamBelabbes <belabbesislam2@gmail.com>
2024-07-28 15:37:11 +0100

    clean up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import CourseCard from "@/components/elements/courseCard/course-card";
-import InfoCard from "@/components/elements/infoCard/info-card";
-
 export default async function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center gap-3 bg-neutral-100 text-white"></main>
+    <main className="flex min-h-screen flex-col items-center justify-center gap-3 bg-neutral-100 p-4 text-white"></main>
   );
 }
