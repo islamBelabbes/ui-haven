32f1ad080868a436c5b20fa7131fbbdbee81784e
islamBelabbes <belabbesislam2@gmail.com>
2024-07-30 18:38:37 +0100

    mark up

--- src/app/categories/[category]/page.tsx
+++ src/app/categories/[category]/page.tsx
@@ -1,4 +1,5 @@
 import ComponentPreviewer from "@/components/elementPreviewer/element-previewer";
+import Installation from "@/components/installation";
 import { type TCategories, categories } from "@/lib/categories";
 import { getElementsByCategory } from "@/lib/elements";
 import React from "react";
@@ -16,9 +17,22 @@ export default async function CategoryPage({
 
   const elements = await getElementsByCategory(category);
   return (
-    <div className="mx-auto grid max-w-7xl">
+    <div className="mx-auto grid max-w-7xl gap-5 p-2">
       {elements.map((element) => (
-        <ComponentPreviewer element={element} key={element.attributes.slug} />
+        <div
+          key={element.attributes.slug}
+          className="overflow-hidden [&:not(:last-child)]:border-b-2 [&:not(:last-child)]:pb-3"
+        >
+          <h1 className="my-3 font-bold">{element.attributes.name}</h1>
+
+          {Boolean(element.attributes.dependencies.external.length) && (
+            <Installation
+              dependencies={element.attributes.dependencies.external}
+            />
+          )}
+
+          <ComponentPreviewer element={element} />
+        </div>
       ))}
     </div>
   );
