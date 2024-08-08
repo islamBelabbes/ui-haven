5b9505a89ddfe8bd84ef583cfd7df23b8a90b2e6
islamBelabbes <belabbesislam2@gmail.com>
2024-08-08 15:22:21 +0100

    add Scroll Area

--- src/components/installation.tsx
+++ src/components/installation.tsx
@@ -1,5 +1,6 @@
 import { codeToHtml } from "shiki";
 import CopyButton from "./copy-button";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
 
 export default async function Installation({
   dependencies,
@@ -12,12 +13,13 @@ export default async function Installation({
     theme: "ayu-dark",
   });
   return (
-    <div className="relative border">
+    <ScrollArea className="relative border">
       <CopyButton text={rawCode} className="top-1/2 -translate-y-1/2" />
       <div
         dangerouslySetInnerHTML={{ __html: code }}
         className="installation"
       />
-    </div>
+      <ScrollBar orientation="horizontal" />
+    </ScrollArea>
   );
 }
