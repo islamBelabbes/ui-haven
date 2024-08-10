217c2c099d5ae34cb1aaa3a0860a0a7b019be28a
islamBelabbes <belabbesislam2@gmail.com>
2024-08-10 15:45:06 +0100

    add footer

--- src/app/(root)/layout.tsx
+++ src/app/(root)/layout.tsx
@@ -1,3 +1,4 @@
+import Footer from "@/components/footer";
 import Header from "@/components/header";
 
 export default function RootLayout({
@@ -7,6 +8,7 @@ export default function RootLayout({
     <>
       <Header />
       {children}
+      <Footer />
     </>
   );
 }
---
+++ src/components/footer.tsx
@@ -0,0 +1,37 @@
+import { REPO_URL } from "@/lib/constants";
+import React from "react";
+
+const RESOURCES = [
+  {
+    id: "github",
+    url: REPO_URL,
+    logo: "github.svg",
+  },
+  {
+    id: "figma",
+    url: "https://www.figma.com/community/file/1124982314884360813",
+    logo: "figma.svg",
+  },
+];
+
+function Footer() {
+  return (
+    <footer className="mt-4 flex justify-between border-t px-3 py-[18px] md:px-20">
+      <span>© 2024 Ping Labs. All rights reserved.</span>
+
+      <ul className="flex gap-2">
+        {RESOURCES.map(({ id, url, logo }) => {
+          return (
+            <li key={id} className="relative size-8">
+              <a href={url} target="_blank" rel="noreferrer">
+                <img src={logo} alt={id} className="absolute h-full w-full" />
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+    </footer>
+  );
+}
+
+export default Footer;
