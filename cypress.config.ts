ce40b8066800ffd34c0d89d7f3edcc230d26f761
islamBelabbes <belabbesislam2@gmail.com>
2024-08-17 16:57:10 +0100

    clean up

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,22 @@
+import { clearSeededElement, seedElement } from "./src/lib/seed";
 import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
     baseUrl: "http://localhost:3000",
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // on("task", {
+      //   async "seed:elements"(options) {
+      //     await seedElement(options);
+      //     return null;
+      //   },
+      // });
+      // on("task", {
+      //   async "seed:clear"() {
+      //     await clearSeededElement();
+      //     return null;
+      //   },
+      // });
     },
   },
 });
