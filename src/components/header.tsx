3ce264d5aa5d33a5a5d46a09cbd77c1b047ccffc
islamBelabbes <belabbesislam2@gmail.com>
2024-08-18 15:27:16 +0100

    add container

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,31 +3,34 @@ import { buttonVariants } from "./ui/button";
 import { motion } from "framer-motion";
 import { fadeIn, transition } from "@/lib/motion";
 import Link from "next/link";
+import Container from "./container";
 
 function Header() {
   return (
-    <header className="flex h-[88px] items-center justify-between border bg-background px-3 py-[18px] md:px-20">
-      <motion.div
-        className="text-3xl font-bold"
-        variants={fadeIn}
-        initial="hidden"
-        animate="visible"
-        transition={transition}
-      >
-        <Link href="/">Ui Haven</Link>
-      </motion.div>
+    <header className="border bg-background">
+      <Container className="flex h-[88px] items-center justify-between">
+        <motion.div
+          className="text-3xl font-bold"
+          variants={fadeIn}
+          initial="hidden"
+          animate="visible"
+          transition={transition}
+        >
+          <Link href="/">Ui Haven</Link>
+        </motion.div>
 
-      {/* Actions */}
-      <motion.a
-        className={buttonVariants({ variant: "default" })}
-        variants={fadeIn}
-        initial="hidden"
-        animate="visible"
-        transition={transition}
-        href="#categories-list"
-      >
-        Get Started
-      </motion.a>
+        {/* Actions */}
+        <motion.a
+          className={buttonVariants({ variant: "default" })}
+          variants={fadeIn}
+          initial="hidden"
+          animate="visible"
+          transition={transition}
+          href="#categories-list"
+        >
+          Get Started
+        </motion.a>
+      </Container>
     </header>
   );
 }
